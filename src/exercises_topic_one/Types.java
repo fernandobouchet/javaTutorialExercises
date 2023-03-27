e5f224b5d6acf717fee517f3edd10a83bf748b87
fernandobouchet <fernandobouchet@gmail.com>
2023-03-26 21:57:54 -0300

    Added first package with 'exercises topic one'

---
+++ src/exercises_topic_one/Types.java
@@ -0,0 +1,37 @@
+package exercises_topic_one;
+
+public class Types {
+    /**
+     * Method to display most commons Java types.
+     * @param args - Data type of the method.
+     */
+    public static void main(String[] args) {
+    // Numerics
+        // Integers
+        byte numericByte = 5;
+        System.out.println(numericByte);
+        short numericShort = 9;
+        System.out.println(numericShort);
+        int numericInt = 12;
+        System.out.println(numericInt);
+        long numericLong = 4654689;
+        System.out.println(numericLong);
+        // Decimals
+        float numericFloat = 12.5f;
+        System.out.println(numericFloat);
+        double numericDouble = 23.4d;
+        System.out.println(numericDouble);
+
+        //Boolean
+        boolean booleanFalse = false;
+        System.out.println(booleanFalse);
+        boolean booleanTrue = true;
+        System.out.println(booleanTrue);
+
+        //Text
+        char textChar = 'z';
+        System.out.println(textChar);
+        String textString = "Hello Java!";
+        System.out.println(textString);
+    }
+}
