e604bf7dc205d0e5535cb4fc466f8d0bf3fc1315
fernandobouchet <fernandobouchet@gmail.com>
2023-03-27 23:54:04 -0300

    Add second exercise

---
+++ src/exercises_topic_two/Functions.java
@@ -0,0 +1,19 @@
+package exercises_topic_two;
+
+public class Functions {
+    public static void main(String[] args) {
+        double price = getIva(100);
+        System.out.println(price);
+    }
+
+    /**
+     * Simple function to calculate IVA
+     * @param price - Price to calculate IVA (21% in Argentina)
+     * @return - Returns the IVA calculated with the price
+     */
+    static double getIva(int price) {
+        return price * 1.21;
+    }
+
+
+}
